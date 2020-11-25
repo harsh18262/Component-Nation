7939aec6d008324cbee32f6818714393d58cd056
harsh18262 <harshwardhanmehrotra@gmail.com>
2020-11-26 00:54:00 +0530

    created models and migrated the database

--- component_nation/home/admin.py
+++ component_nation/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Prebuilt_Base,Prebuilt_70k,Prebuilt_80k,Prebuilt_90k,Prebuilt_1L
 # Register your models here.
+
+admin.site.register(Prebuilt_Base)
+admin.site.register(Prebuilt_70k)
+admin.site.register(Prebuilt_80k)
+admin.site.register(Prebuilt_90k)
+admin.site.register(Prebuilt_1L)
--- component_nation/home/models.py
+++ component_nation/home/models.py
@@ -2,6 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-#class Person(models.Model):
-    #product = models.TextField()
-    #desc = models.TextField()
+class Prebuilt_Base(models.Model):
+        id=models.AutoField(primary_key=True)
+        product = models.TextField(default=" ")
+        desc = models.TextField(default=" ")
+        img = models.ImageField(upload_to='Pre_built/images',default='default.jpg')
+        url=models.URLField(default=" ")
+        price=models.CharField(max_length=20,default=0)
+
+class Prebuilt_70k(Prebuilt_Base):
+    pass
+
+class Prebuilt_80k(Prebuilt_Base):
+    pass
+
+class Prebuilt_90k(Prebuilt_Base):
+    pass
+
+class Prebuilt_1L(Prebuilt_Base):
+    pass
